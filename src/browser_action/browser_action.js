80fe757f7898bb5f2c1997b7fbb5c027044db941
Valerie Huang <valeriewhuang@gmail.com>
2021-01-07 23:13:57 -0500

    first commit

---
+++ src/browser_action/browser_action.js
@@ -0,0 +1,27 @@
+const button = document.querySelector(#add-item)
+button.addEventListener('onClick', function() {
+
+})
+
+/*
+create object of objects to hold list of items
+each object = item with custom label/name of product, URL, image category?
+push object into array on click
+
+have a button to remove an item
+- when it's clicked, delete object 
+
+Share feature
+- email or post to social media
+
+option to create multiple lists
+organize variations/iterations of items using nested object
+
+
+
+classes to organize code
+  -product
+  -list 
+
+
+/*
